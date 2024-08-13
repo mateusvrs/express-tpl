e88902a2afd751e638481a893d00463f1f018e9c
mateusvrs <mateusvrs.dev@gmail.com>
2024-08-13 11:56:51 -0300

    lint: improve rules with ident, quotes + others

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,19 +4,23 @@ import stylistic from "@stylistic/eslint-plugin"
 import tseslint from "typescript-eslint"
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: {
-      globals: globals.browser,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        rules: {
+            "no-console": "error",
+            "no-duplicate-imports": "error",
+            "@stylistic/semi": ["error", "never"],
+            "@stylistic/quotes": ["warn", "double"],
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/brace-style": ["warn", "1tbs"],
+        },
+        plugins: {
+            "@stylistic": stylistic,
+        },
     },
-    rules: {
-      "no-console": "error",
-      "@stylistic/semi": ["error", "never"],
-    },
-    plugins: {
-      "@stylistic": stylistic,
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ]
